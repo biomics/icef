b1a798596e76b53db618a4ea2ddf613853dc7989
daniel.schreckling <ds@sec.uni-passau.de>
2016-03-24 16:06:23 +0100

    Replaced tabs with spaces

--- org.coreasm.engine/src/org/coreasm/engine/mailbox/Mailbox.java
+++ org.coreasm.engine/src/org/coreasm/engine/mailbox/Mailbox.java
@@ -1,4 +1,4 @@
-/*	
+/* 
  * Copyright (C) 2016 BIOMICS project (Eric Rothstein, Daniel Schreckling)
  * 
  * Licensed under the Academic Free License version 3.0 
@@ -14,7 +14,7 @@ import java.util.Set;
 import org.coreasm.engine.absstorage.MessageElement;
 
 /** 
- *	Defines the interface of the mailbox
+ * Defines the interface of the mailbox
  *   
  *  @author Eric Rothstein
  *  @author Daniel Schreckling
@@ -23,41 +23,41 @@ import org.coreasm.engine.absstorage.MessageElement;
 public interface Mailbox {
 
     /**
-	 * Returns a the message elements in the outbox,
+     * Returns a the message elements in the outbox,
      * which wait for beeing sent. After the execution
      * of this instruction, the outbox is empty.
      *
-	 * @return Set of MessageElements in the outbox
-	 */
-	public Set<MessageElement> emptyOutbox();
-
+     * @return Set of MessageElements in the outbox
+     */
+    public Set<MessageElement> emptyOutbox();
+    
     /**
-	 * Puts the Set of MessageElements into the Inbox
+     * Puts the Set of MessageElements into the Inbox
      * of this coreASM
      *
      * @param msgs The message arriving at the mailbox which need to be stored in the Inbox
-	 * @return Set of MessageElements which already existed in the Inbox
-	 */
-	public Set<MessageElement> fillInbox(Set<MessageElement> msgs);
-
-	/**
-	 * Returns a the message elements in the inbox
-	 * @return the message elements in the inbox
-	 */
-	public Set<MessageElement> getInbox();
-	
-	/**
-	 * Returns a the message elements in the inbox
-	 * @return the message elements in the inbox
-	 */
-	public void sendMessage(MessageElement message);
-	
-	/**
-	 * Returns an instance of this mailbox registered
-	 * for the running thread.
-	 */
-	public Mailbox getMailboxInstance();
-	
+     * @return Set of MessageElements which already existed in the Inbox
+     */
+    public Set<MessageElement> fillInbox(Set<MessageElement> msgs);
+    
+    /**
+     * Returns a the message elements in the inbox
+     * @return the message elements in the inbox
+     */
+    public Set<MessageElement> getInbox();
+    
+    /**
+     * Returns a the message elements in the inbox
+     * @return the message elements in the inbox
+     */
+    public void sendMessage(MessageElement message);
+    
+    /**
+     * Returns an instance of this mailbox registered
+     * for the running thread.
+     */
+    public Mailbox getMailboxInstance();
+    
     
     /**
      * Cleans up any cached data in the mailbox.
