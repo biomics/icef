dddfef99268a6c4bfdd7a71ee5452fb665c14b48
daniel.schreckling <ds@sec.uni-passau.de>
2016-04-14 00:19:47 +0200

    - Enable message passing between standalone wrappers

--- org.coreasm.biomics.wrapper/src/main/java/org/coreasm/biomics/EngineManager.java
+++ org.coreasm.biomics.wrapper/src/main/java/org/coreasm/biomics/EngineManager.java
@@ -2,7 +2,23 @@ package org.coreasm.biomics;
 
 import java.util.HashMap;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import javax.ws.rs.ProcessingException;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+
 public class EngineManager {
+
+    private static Wrapper wrapper = null;
 
     private static final HashMap<String, CoreASMContainer> engines = 
         new HashMap<>();
@@ -16,13 +32,41 @@ public class EngineManager {
         engines.put(req.name, casm);
     }
 
+    public static boolean pauseEngine(String name) {
+        if(engines.containsKey(name)) {
+            CoreASMContainer casm = engines.get(name);
+            try {
+                casm.wait();
+            } catch (InterruptedException e) {
+                // TODO Acount for this.
+                System.err.println("Unable to put engine to sleep");
+            }
+            
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean resumeEngine(String name) {
+        if(engines.containsKey(name)) {
+            CoreASMContainer casm = engines.get(name);
+            casm.notify();
+            
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static boolean startEngine(String name) {
-        System.out.println("Create an existing engine");
+        System.out.println("Start an existing engine");
         System.out.println("AgentName: "+name);
 
         if(engines.containsKey(name)) {
             CoreASMContainer casm = engines.get(name);
-            casm.exec();
+            casm.start();
+            
             return true;
         } else {
             return false;
@@ -30,19 +74,86 @@ public class EngineManager {
     }
 
     public static boolean receiveMsg(MessageRequest req) {
-        System.out.println("Engine Manager receives message");
-        System.out.println("Receiver: "+req.receiver);
-        System.out.println("Sender: "+req.sender);
-        System.out.println("Body: "+req.body);
+        /* System.out.println("Engine Manager receives message");
+           System.out.println("Receiver: "+req.toAgent);
+           System.out.println("Sender: "+req.fromAgent);
+           System.out.println("Body: "+req.body);
+        */
 
-        return true;
+        // this is an agent message and must be 
+        // forwarded to the correct agent
+        if(req.type.equals("agent")) {
+            // System.out.println("try to forward msg to CoreASM");
+
+            String agent = "";
+
+            if(wrapper.config.managerHost != null) {
+                // check whether this agent is managed here
+                String[] names = req.toAgent.split(":");
+                if(names.length > 1)
+                    agent = names[1];
+                else
+                    agent = names[0];
+                
+                if(engines.containsKey(agent)) {
+                    System.out.println("EngineManager knows this agent");
+                } else {
+                    System.out.println("EngineManager does not know this agent");
+                    return false;
+                }
+                req.toAgent = agent;
+
+                CoreASMContainer trg = engines.get(agent);
+                trg.receiveMsg(req);
+            } else {
+                // TODO - forward the message to all possible agents that match specified target
+                for(CoreASMContainer trg : engines.values()) {
+                    trg.receiveMsg(req);
+                }
+            }
+
+            return true;
+        }
+
+        System.out.println("EngineManager receives unknown message type. Ignore!");
+
+        return false;
+    }
+
+    public static void sendMsg(MessageRequest req) {
+        if(wrapper == null || wrapper.commUrl == null) {
+            System.err.println("ERROR: Unable to send message. Target unknown!");
+            System.exit(1);
+        }
+
+        String json = MessageRequest.getJSON(req);
+
+        try {
+            Response response = ClientBuilder.newBuilder()
+                // .register(JacksonFeature.class) // would be the better way but did not get it working
+                .build()
+                .target(wrapper.commUrl)
+                .path("message")
+                .request(MediaType.APPLICATION_JSON)
+                .accept("*/*")
+                .post(Entity.json(json));
+            // System.out.println("response.getStatusCode: "+response);
+        } 
+        catch (ProcessingException pe) {
+            System.out.println("Problem processing: "+pe);
+            System.out.println("Cause: "+pe.getCause());
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
     }
     
     public static void stopAll() {
         
     }
 
-    public static void reset() {
+    public static void reset(Wrapper wrapper) {
+        EngineManager.wrapper = wrapper;
+
         engines.clear();
     }
 }
