0b0424be99ba398b34ab72a1d70388ad830e1157
Eric Rothstein <er@sec.uni-passau.de>
2016-05-23 22:37:55 +0200

    Clearing the agentsTo sets

--- org.coreasm.engine/src/org/coreasm/engine/Engine.java
+++ org.coreasm.engine/src/org/coreasm/engine/Engine.java
@@ -1414,6 +1414,9 @@ public class Engine implements ControlAPI {
 
 				case ecAggregate:
 					agentsToCreate.clear();
+					agentsToDelete.clear();
+					agentsToDeregister.clear();
+					agentsToRegister.clear();
 					next(EngineMode.emAggregation);
 					break;
 
