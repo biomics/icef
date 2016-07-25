1326b5dd7d1fa15460de212e30025b8ca9949c79
Daniel Schreckling <ds@sec.uni-passau.de>
2016-07-25 16:30:40 +0200

    - Improved REST interface

--- org.coreasm.biomics.wrapper/src/org/coreasm/biomics/ASIMResource.java
+++ org.coreasm.biomics.wrapper/src/org/coreasm/biomics/ASIMResource.java
@@ -1,3 +1,16 @@
+/*
+ * ASIMResource.java v1.0
+ *
+ * This file contains source code developed by the European
+ * FP7 research project BIOMICS (Grant no. 318202)
+ * Copyright (C) 2016 Daniel Schreckling
+ *
+ * Licensed under the Academic Free License version 3.0
+ *   http://www.opensource.org/licenses/afl-3.0.php
+ *
+ *
+ */
+
 package org.coreasm.biomics;
 
 import javax.ws.rs.GET;
@@ -21,43 +34,59 @@ import org.coreasm.engine.CoreASMError;
 @Path("asims")
 public class ASIMResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Path("/{asimName}")
-    @Produces("text/plain")
-    public String getASIMStatus(@PathParam("asimName") String asimName) {
-        System.out.println("asimName: '"+asimName+"'");
+    // TODO replace all string responses with real JSON mappings to Java Objects
 
-        return "Got it!";
+    @GET
+    @Produces("application/json")
+    public Response getAllASIMs() {
+        String response = EngineManager.getASIMs();
+        return Response.status(200).entity(response).type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Path("/{simId}")
+    @Produces("application/json")
+    public Response getASIMStatus(@PathParam("simId") String simulation ) {
+        String response = EngineManager.getASIMs(simulation);
+        
+        if(response != null)
+            return Response.status(200).entity(response).type(MediaType.APPLICATION_JSON).build();
+        else
+            return Response.status(404).entity("{ 'error' : 'Brapper does not host any ASIM for this simulation.'}").type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Path("/{simId}/{asimName}")
+    @Produces("application/json")
+    public Response getASIMStatus(@PathParam("simId") String simulation, @PathParam("asimName") String asimName) {
+        String response = EngineManager.getASIM(simulation, asimName);
+        
+        if(response != null)
+            return Response.status(200).entity(response).type(MediaType.APPLICATION_JSON).build();
+        else
+            return Response.status(404).entity("{ 'error' : 'Brapper does not host this ASIM in the specified simulation.'}").type(MediaType.APPLICATION_JSON).build();
     }
 
     @PUT
-    @Path("/{simId}/{asimName}")
+    @Path("/{simId}/{asimName}/{command}")
     @Consumes("application/json")
     @Produces("application/json")
-    public Response controlASIM(@PathParam("simId") String simId, @PathParam("asimName") String asimName, String controlParameters) {
+    public Response controlASIM(@PathParam("simId") String simId, @PathParam("asimName") String asimName, @PathParam("command") String command) {
         ObjectMapper mapper = new ObjectMapper();
-        ASIMControlRequest req = null;
-        try {
-            req = mapper.readValue(controlParameters, ASIMControlRequest.class);
-        } catch (IOException ioe) {
-            System.err.println("Invalid control request: '"+controlParameters+"'");
-            System.err.println(ioe);
-        }
 
-        boolean success = EngineManager.controlASIM(simId, asimName, req.command);
+        int code = EngineManager.controlASIM(simId, asimName, command);
 
-        String error = null;
-        if(!success) {
-            error = "Unable to '" + req.command + "' ASIM.";
-        }
+        String error = "";
+	if(code != 200) {
+	    if(code == 404)
+		error = "Cannot execute command '"+command+"'. Unable to find specified simulation or ASIM";
+	    else
+		if(code == 400)
+		    error = "Unable to control ASIM with command '" + command + "'. Unknown command!";
+	    return Response.status(code).entity(" { \"error\" : \""+error+"\" } ").type(MediaType.APPLICATION_JSON).build();
+	}
 
-        ASIMControlResponse res = new ASIMControlResponse(asimName, simId, error);
+        ASIMControlResponse res = new ASIMControlResponse(asimName, simId, EngineManager.getASIMState(simId, asimName), error);
 
         String json = "{}";
         try {
@@ -70,7 +99,7 @@ public class ASIMResource {
             e.printStackTrace();
         }
 
-        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+	return Response.status(code).entity(json).type(MediaType.APPLICATION_JSON).build();
     }
 
     @DELETE
@@ -78,26 +107,15 @@ public class ASIMResource {
     @Consumes("application/json")
     public Response destroyASIM(@PathParam("simId") String simId, @PathParam("asimName") String asimName) {
         if(EngineManager.destroyASIM(simId, asimName))
-            return Response.status(200).build();
+            return Response.status(200).entity("{ \"msg\" : \"ASIM successfully deleted.\" }").type(MediaType.APPLICATION_JSON).build();
         else
-            return Response.status(406).build();
-    }
-
-    @GET
-    @Produces("text/plain")
-    public String getAllASIMs() {
-        System.out.println("All currently running ASIMs");
-
-        return "Got it!";
+            return Response.status(404).entity("{ \"error\" : \"ASIM or Simulation not found.\" }").type(MediaType.APPLICATION_JSON).build();
     }
 
     @PUT
     @Consumes("application/json")
     @Produces("application/json")
     public Response createNewASIM(String createParameters) {
-        System.out.println("Create new ASIM");
-        System.out.println("JSON: "+createParameters);
-
         ObjectMapper mapper = new ObjectMapper();
         
         ASIMCreationRequest req = null;
@@ -106,16 +124,23 @@ public class ASIMResource {
         } catch (IOException ioe) {
             System.err.println("Invalid creation request: '"+createParameters+"'");
             System.err.println(ioe);
+	    return Response.status(400).entity("{ msg : \"Unable to create ASIM. JSON specification of ASIM is incorrect: "+ioe+"\" }").type(MediaType.APPLICATION_JSON).build();
         }
 
         CoreASMError error = EngineManager.createASIM(req);
 
-        ASIMCreationResponse res = new ASIMCreationResponse(req.name, req.simulation, error == null ? true : false, error == null ? "" : error.toString());
-
+        ASIMCreationResponse res = new ASIMCreationResponse();
+	res.setMsg("ASIM successfully created");
+	res.asim.setName(req.name);
+	res.asim.setSimulation(req.simulation);
+	res.asim.setStatus("unknown");
+	res.asim.setSuccess(error == null ? true : false);
+	res.asim.setError(error == null ? "" : error.toString());
+	
         String json = "{}";
 
         try {
-            json  = mapper.writeValueAsString(res);
+            json = mapper.writeValueAsString(res);
         }  catch (JsonGenerationException e) {
             e.printStackTrace();
         } catch (JsonMappingException e) {
@@ -124,6 +149,11 @@ public class ASIMResource {
             e.printStackTrace();
         }
 
-        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+	System.err.println("RESPONSE TO SEND: "+json);
+
+        if(error != null)
+            return Response.status(400).entity(json).type(MediaType.APPLICATION_JSON).build();
+        else
+            return Response.status(201).entity(json).type(MediaType.APPLICATION_JSON).build();
     }
 }
