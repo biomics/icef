d86dc8296eb55cf330159fb8569284a91ff20aeb
Daniel Schreckling <ds@sec.uni-passau.de>
2016-07-25 17:16:53 +0200

    - added Copyright notice

--- org.coreasm.biomics.wrapper/src/org/coreasm/biomics/serializers/RuleElementSerializer.java
+++ org.coreasm.biomics.wrapper/src/org/coreasm/biomics/serializers/RuleElementSerializer.java
@@ -1,3 +1,16 @@
+/*
+ * RuleElementSerializer.java v1.0
+ *
+ * This file contains source code developed by the European
+ * FP7 research project BIOMICS (Grant no. 318202)
+ * Copyright (C) 2016 Daniel Schreckling
+ *
+ * Licensed under the Academic Free License version 3.0
+ *   http://www.opensource.org/licenses/afl-3.0.php
+ *
+ *
+ */
+
 package org.coreasm.biomics.serializers;
 
 import java.io.IOException;
