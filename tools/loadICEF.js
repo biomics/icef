9c925a181cb29fa47e67117fa48d931fb5fec735
Daniel Schreckling <ds@sec.uni-passau.de>
2016-08-09 18:20:33 +0200

    Fix a problem with startsWith which would not run on a new installation

--- tools/loadICEF.js
+++ tools/loadICEF.js
@@ -34,74 +34,74 @@ function reloadJSON(icef, baseDir) {
     var toload = [];
     
     for(var prop in icef) {
-	switch(prop) {
-	case "asims" :
-	case "schedulers" :
-	    var asims = icef[prop];
-	    if(!(asims instanceof Array)) {
-		console.log("Invalid ICEF specification. Expected array of ASIM specifications in '"+prop+"'.");
-	    } else {
-		for(var i in asims) {
-		    var asim = asims[i];
-		    if(asim != undefined && asim.file != undefined) {
-			toload.push({ file : asim.file, index : i, prop : prop, start : asim.start });
-		    }
-		}
-	    }
-	    break;
-	}
+	      switch(prop) {
+	      case "asims" :
+	      case "schedulers" :
+	          var asims = icef[prop];
+	          if(!(asims instanceof Array)) {
+		            console.log("Invalid ICEF specification. Expected array of ASIM specifications in '"+prop+"'.");
+	          } else {
+		            for(var i in asims) {
+		                var asim = asims[i];
+		                if(asim != undefined && asim.file != undefined) {
+			                  toload.push({ file : asim.file, index : i, prop : prop, start : asim.start });
+		                }
+		            }
+	          }
+	          break;
+	      }
     }
     
     for(var i in toload) {
-	var file = toload[i].file;
-	var index = toload[i].index;
-	var prop = toload[i].prop;
-	var start = toload[i].start;
+	      var file = toload[i].file;
+	      var index = toload[i].index;
+	      var prop = toload[i].prop;
+	      var start = toload[i].start;
 
-	if(!file.startsWith("/")) {
-	    file = baseDir + "/" + file;
-	}
-	
-	data = new String(fs.readFileSync(file));
+	      if(file[0] != "/") {
+	          file = baseDir + "/" + file;
+	      }
+	      
+	      data = new String(fs.readFileSync(file));
 
-	if(data == null) {
-	    return null;
-	} else {
-	    var newASIM = {};
-	    
-	    data = data.replace(/\/\/.+/g, "");
-	    data = data.replace(/[ \n]*use .+/g, "");
-	    
-	    newASIM.name = /[ \n]*CoreASIM (.+)\n/g.exec(data)[1];
-	    if(newASIM.name == null)
-		newASIM.name = "undefined";
-	    newASIM.init = /[ \n]*init (.+)\n/g.exec(data)[1];
-	    if(newASIM.init == null)
-		newASIM.init = "skip";
-	    newASIM.policy = /[ \n]*scheduling (.+)\n/g.exec(data)[1];
-	    if(newASIM.policy == null)
-		newASIM.policy = "skip";
-	    
-	    // guess the init rule and extract program(self)
-	    var initRuleExp = new RegExp("rule[ \t]+"+newASIM.init+".*=(.|[\n\r])+?(rule|derived|controlled|universe)", "m");
-	    var initRule = initRuleExp.exec(data)[0];
-	    newASIM.program = /program\(self\) *:= *(.+?)\n/g.exec(initRule)[1];
-	    if(newASIM.program == null)
-		newASIM.program = "skip";
-	    
-	    data = data.replace(/[ \n]*CoreASIM (.+)\n/g, "");
-	    data = data.replace(/[ \n]*init (.+)\n/g, "");
-	    data = data.replace(/[ \n]*scheduling (.+)\n/g, "");
-	    data = data.replace(/^[ \n]*/m, "");
-	    data = data.replace(/[ \n]*$/m, "");
-	    
-	    newASIM.signature = data;
-	    
-	    if(start != undefined)
-		newASIM.start = start;
-	    
-	    icef[prop][index] = newASIM;
-	}
+	      if(data == null) {
+	          return null;
+	      } else {
+	          var newASIM = {};
+	          
+	          data = data.replace(/\/\/.+/g, "");
+	          data = data.replace(/[ \n]*use .+/g, "");
+	          
+	          newASIM.name = /[ \n]*CoreASIM (.+)\n/g.exec(data)[1];
+	          if(newASIM.name == null)
+		            newASIM.name = "undefined";
+	          newASIM.init = /[ \n]*init (.+)\n/g.exec(data)[1];
+	          if(newASIM.init == null)
+		            newASIM.init = "skip";
+	          newASIM.policy = /[ \n]*scheduling (.+)\n/g.exec(data)[1];
+	          if(newASIM.policy == null)
+		            newASIM.policy = "skip";
+	          
+	          // guess the init rule and extract program(self)
+	          var initRuleExp = new RegExp("rule[ \t]+"+newASIM.init+".*=(.|[\n\r])+?(rule|derived|controlled|universe)", "m");
+	          var initRule = initRuleExp.exec(data)[0];
+	          newASIM.program = /program\(self\) *:= *(.+?)\n/g.exec(initRule)[1];
+	          if(newASIM.program == null)
+		            newASIM.program = "skip";
+	          
+	          data = data.replace(/[ \n]*CoreASIM (.+)\n/g, "");
+	          data = data.replace(/[ \n]*init (.+)\n/g, "");
+	          data = data.replace(/[ \n]*scheduling (.+)\n/g, "");
+	          data = data.replace(/^[ \n]*/m, "");
+	          data = data.replace(/[ \n]*$/m, "");
+	          
+	          newASIM.signature = data;
+	          
+	          if(start != undefined)
+		            newASIM.start = start;
+	          
+	          icef[prop][index] = newASIM;
+	      }
     }
 
     return icef;
@@ -109,17 +109,17 @@ function reloadJSON(icef, baseDir) {
 
 jsonfile.readFile(specification, function(err, icef) {
     if(err != null) {
-	console.log("Error: "+err);
+	      console.log("Error: "+err);
     } else {
-	var baseDir = path.dirname(specification);
-	    
-	var newICEF = reloadJSON(icef, baseDir);
+	      var baseDir = path.dirname(specification);
+	      
+	      var newICEF = reloadJSON(icef, baseDir);
 
-	console.log("Load the following specification:");
-	console.log(newICEF);
+	      console.log("Load the following specification:");
+	      console.log(newICEF);
 
-	var data = JSON.stringify(icef);
-	
+	      var data = JSON.stringify(icef);
+	      
         var options = {
             host: trgHost,
             port: trgPort,
@@ -134,24 +134,24 @@ jsonfile.readFile(specification, function(err, icef) {
         var self = this;
         var req = http.request(options, function(res) {
             var resData = "";
-	    
+	          
             res.setEncoding('utf8');
-	    
+	          
             res.on('data', function(chunk) {
                 if(chunk)
                     resData += chunk;
             });
-	    
+	          
             // TODO: create error in brapper which shows that something went wrong
             res.on('end', function(chunk) {
                 if(chunk)
                     resData += chunk;
-		
+		            
                 if(res.statusCode != 201) {
                     console.log("Specification not loaded!");
-		    console.log("Problem: ", resData);
+		                console.log("Problem: ", resData);
                 } else {
-		    var result = null;
+		                var result = null;
                     try {
                         result = JSON.parse(resData);
                         console.log("Specification loaded successfully.");
@@ -161,13 +161,12 @@ jsonfile.readFile(specification, function(err, icef) {
                 }
             });
         });
-	
+	      
         req.on('error', function(e) {
             console.log("Unable to load ICEF specifiation: "+e);
         });
-	
+	      
         req.write(data);
         req.end();
     }
 });
-
